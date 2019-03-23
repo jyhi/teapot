47d9678b8f1fba3dd1d67c9d1b88d95d33f3e056
Junde Yhi <lmy441900@aosc.xyz>
2019-03-24 00:06:40 +0800

    server.c, config.h: add binding address option
    This allows a user to adjust binding address.

--- src/config.h
+++ src/config.h
@@ -12,6 +12,11 @@
 #define TEAPOT_VERSION "0.1"
 
 /**
+ * Define default binding address of Teapot.
+ */
+#define TEAPOT_DEFAULT_BIND_ADDRESS "127.0.0.1"
+
+/**
  * Define default binding ports of Teapot.
  */
 #define TEAPOT_DEFAULT_HTTP_PORT  8080
--- src/server.c
+++ src/server.c
@@ -3,6 +3,7 @@
 #include "config.h"
 
 // Ports to bind on
+static gchar  *address    = TEAPOT_DEFAULT_BIND_ADDRESS;
 static guint16 http_port  = TEAPOT_DEFAULT_HTTP_PORT;
 static guint16 https_port = TEAPOT_DEFAULT_HTTPS_PORT;
 
@@ -15,6 +16,8 @@ int teapot_handle_options(GApplication *app, GVariantDict *opts, gpointer data)
     puts(TEAPOT_NAME " " TEAPOT_VERSION);
     return 0; // no future action is needed, exit
   }
+
+  g_variant_dict_lookup(opts, "bind", "s", &address);
 
   if (g_variant_dict_lookup(opts, "http-port", "i", &temp_port)) {
     // Port number is actually from 1 to 65535
@@ -38,6 +41,7 @@ int teapot_handle_options(GApplication *app, GVariantDict *opts, gpointer data)
 
   // BUG: 0 is not catched by the command line argument parser, so setting a
   // port to 0 will fall directly to the default ones. I don't know why.
+  g_debug("Binding address set to %s", address);
   g_debug("HTTP port set to %d%s", http_port, http_port == TEAPOT_DEFAULT_HTTP_PORT ? " (default)" : "");
   g_debug("HTTPS port set to %d%s", https_port, https_port == TEAPOT_DEFAULT_HTTPS_PORT ? " (default)" : "");
 
@@ -60,6 +64,7 @@ int teapot_run(int argc, char **argv)
 
   // Adds description to the help manual (--help)
   g_application_set_option_context_parameter_string(app, "- a simple HTTP(S) server");
+  g_application_add_main_option(app, "bind", 'b', G_OPTION_FLAG_NONE, G_OPTION_ARG_STRING, "Address to bind", "address");
   g_application_add_main_option(app, "http-port", 'p', G_OPTION_FLAG_NONE, G_OPTION_ARG_INT, "Port to bind the HTTP service", "port");
   g_application_add_main_option(app, "https-port", 'P', G_OPTION_FLAG_NONE, G_OPTION_ARG_INT, "Port to bind the HTTPS service", "port");
   g_application_add_main_option(app, "version", 'v', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, "Show version string", NULL);
