2f0093f9425cbe50ade30d3ef78c0d1f5bf08deb
Junde Yhi <lmy441900@aosc.xyz>
2019-03-25 16:04:20 +0800

    http.h: fix typo

--- src/http.h
+++ src/http.h
@@ -2,7 +2,7 @@
 #define TEAPOT_HTTP_H
 
 /**
- * Given an HTTP request string, process it, and give a HTTP output.
+ * Given an HTTP request string, process it, and give an HTTP output.
  *
  * @param size  [out] The size of the returned HTTP response
  * @param input [in]  The HTTP request given by the client
