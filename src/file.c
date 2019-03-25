d60e46a77c5ab0fe99aa7bb539e58eedd166831c
Junde Yhi <lmy441900@aosc.xyz>
2019-03-25 21:14:03 +0800

    file.c: implement teapot_file_read

--- src/file.c
+++ src/file.c
@@ -1,4 +1,4 @@
-#include <glib.h>
+#include <gio/gio.h>
 #include "file.h"
 
 /********** Private APIs **********/
@@ -31,7 +31,64 @@ void teapot_file_free(struct TeapotFile *file)
 
 struct TeapotFile *teapot_file_read(const char *path)
 {
-  // unimplemented
+  GError  *error = NULL;
+  gboolean r     = FALSE;
+
+  // For security consideration, we do not allow file access in parent directories
+  gchar *abspath = g_canonicalize_filename(path + 1, NULL);
+  g_debug("File: canonicalized filename: %s", abspath);
+  if (!g_str_has_prefix(abspath, g_get_current_dir())) {
+    g_message("File: requested path goes out of scope, reject");
+    g_free(abspath);
+    return NULL;
+  }
+
+  GFile *file = g_file_new_for_path(abspath);
+
+  // TODO: currently we do not support directory listing
+  GFileType file_type = g_file_query_file_type(file, G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS, NULL);
+  if (file_type != G_FILE_TYPE_REGULAR) {
+    g_message("File: requested path is not a regular file, reject");
+    g_clear_object(&file);
+    g_free(abspath);
+    return NULL;
+  }
+
+  // Query file information
+  GFileInfo *info = g_file_query_info(file, "standard::*", G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS, NULL, &error);
+  if (!info) {
+    g_warning("Failed to query file info: %s", error->message);
+    g_clear_error(&error);
+    g_clear_object(&file);
+    return NULL;
+  }
+
+  struct TeapotFile *ret = teapot_file_new();
+
+  ret->filename     = g_strdup(g_file_info_get_name(info));
+  ret->content_type = g_strdup(g_file_info_get_content_type(info));
+
+  g_debug("File: %s is of type %s", ret->filename, ret->content_type);
+
+  // Read the whole file into memory
+  // FIXME
+  r = g_file_load_contents(file, NULL, (char **)&(ret->content), &(ret->size), NULL, &error);
+  if (!r) {
+    g_warning("Failed to load file into memory: %s", error->message);
+    g_clear_error(&error);
+    teapot_file_free(ret);
+    g_clear_object(&info);
+    g_clear_object(&file);
+    return NULL;
+  }
+
+  g_debug("File: loaded %lu bytes", ret->size);
+
+  // Free unused memory
+  g_clear_object(&info);
+  g_clear_object(&file);
+
+  return ret;
 }
 
 bool teapot_file_write(const uint8_t *content, const size_t size, const char *path)
