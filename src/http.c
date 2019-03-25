e72bd0f3f4e6315fb1dd51df590fbd7c67356cd8
YvonneTech <47659990+YvonneTech@users.noreply.github.com>
2019-03-25 20:00:30 +0800

    Update http.c
    Finish the teapot_http_response_construct(); add http_method_to_string() and http_status_to_string()
    Delete static const char *http_put   = "PUT", add static const char *http_head   = "HEAD";

--- src/http.c
+++ src/http.c
@@ -106,7 +106,7 @@ static const char *http_status_method_not_allowed     = HTTP_VERSION " 405 Metho
 static const char *http_status_internal_server_error = HTTP_VERSION " 500 Server Internal Error";
 
 static const char *http_get    = "GET";
-static const char *http_put    = "PUT";
+static const char *http_head   = "HEAD";
 static const char *http_post   = "POST";
 static const char *http_delete = "DELETE";
 
@@ -114,6 +114,93 @@ static const char *htcpcp_status_i_am_a_teapot = HTCPCP_VERSION " 418 I'm a teap
 static const char *htcpcp_brew = "BREW";
 
 /********** Private APIs **********/
+
+/**
+ * Convert enumeration HttpMethod to string.
+ *
+ * @param method [in] HttpMethod to convert.
+ * @return A string constant representing the HTTP method. The caller should not
+ *         free the string!
+ */
+static const char *http_method_to_string(enum HttpMethod method)
+{
+  const char *ret = NULL;
+
+  switch (method) {
+    case HTTP_GET:
+      ret = http_get;
+      break;
+    case HTTP_HEAD:
+      ret = http_head;
+      break;
+    case HTTP_POST:
+      ret = http_post;
+      break;
+    case HTTP_DELETE:
+      ret = http_delete;
+      break;
+    case HTCPCP_BREW:
+      ret = htcpcp_brew;
+      break;
+    default:
+      break; // Remain ret to be NULL
+  }
+
+  return ret;
+}
+
+/**
+ * Convert enumeration HttpStatusCode to string.
+ *
+ * @param method [in] HttpStatusCode to convert.
+ * @return A string constant representing the HTTP status. The caller should not
+ *         free the string!
+ */
+static const char *http_status_to_string(enum HttpStatusCode status)
+{
+  const char *ret = NULL;
+
+  switch (status) {
+    case HTTP_STATUS_OK:
+      ret = http_status_ok;
+      break;
+    case HTTP_STATUS_NO_CONTENT:
+      ret = http_status_no_content;
+      break;
+
+    case HTTP_STATUS_MOVED_PERMANENTLY:
+      ret = http_status_moved_permanently;
+      break;
+    case HTTP_STATUS_FOUND:
+      ret = http_status_found;
+      break;
+
+    case HTTP_STATUS_BAD_REQUEST:
+      ret = http_status_bad_request;
+      break;
+    case HTTP_STATUS_FORBIDDEN:
+      ret = http_status_forbidden;
+      break;
+    case HTTP_STATUS_NOT_FOUND:
+      ret = http_status_not_found;
+      break;
+    case HTTP_STATUS_METHOD_NOT_ALLOWED:
+      ret = http_status_method_not_allowed;
+      break;
+
+    case HTTP_STATUS_INTERNAL_SERVER_ERROR:
+      ret = http_status_internal_server_error;
+      break;
+
+    case HTCPCP_STATUS_I_AM_A_TEAPOT:
+      ret = htcpcp_status_i_am_a_teapot;
+      break;
+
+    default:
+      break; // Remain ret to be NULL
+  }
+  return ret;
+}
 
 /**
  * Parse an HTTP string into `struct HttpRequest` for future processing.
@@ -134,12 +221,90 @@ static struct HttpRequest teapot_http_request_parse(const char *http)
  */
 static char *teapot_http_response_construct(const struct HttpResponse response)
 {
-  // unimplemented
+    size_t response_size = 0;
+
+    // Convert the integer content_length into string
+    char buffer[8]; // < For store the content-length
+    snprintf(buffer, 8, "%d", response.content_length);
+
+    // Calculate the buffer size to allocate
+    response_size += strlen(response.version);
+    response_size += strlen(http_status_to_string(response.status_code)) + strlen("\n");
+    
+    response_size += strlen("Content-Type: ") + strlen(response.content_type) + strlen("\n");
+    response_size += strlen("Content-Length: ") + strlen(buffer) + strlen("\n");
+    if (response.connection) {
+      response_size += strlen("Connection: ") + strlen(response.connection) + strlen("\n");
+    }
+    if (response.location) {
+      response_size += strlen("Location: ") + strlen(response.location) + strlen("\n");
+    }
+    if (response.allow) {
+      response_size += strlen("Allow: ") + strlen(response.allow);
+    }
+    response_size += strlen("\r\n");
+    
+    if (response.content) {
+      response_size += strlen(response.content) + strlen("\n");
+    }
+
+    char *output = (char*)malloc(sizeof(char) * response_size + 1);
+    // NOTE: This is required for strcat() to work properly!
+    output[0] = '\0';
+
+    // The first line
+    strcat(output, response.version);
+    strcat(output, http_status_to_string(response.status_code));
+
+    // Header
+    strcat(output, "\nContent-Type: ");
+    strcat(output, response.content_type);
+
+    strcat(output, "\nContent-Length: ");
+    strcat(output, buffer);
+
+    if (response.connection) {
+      strcat(output, "\nConnection: ");
+      strcat(output, response.connection);
+    }
+    if (response.location) {
+      strcat(output, "\nLocation: ");
+      strcat(output, response.location);
+    }
+    if (response.allow) {
+      strcat(output, "\nAllow: ");
+      strcat(output, response.allow);
+    }
+    strcat(output, "\r\n");
+    // Content
+    if (response.content) {
+      strcat(output, response.content);
+      // strcat(output, "\0");
+    }
+    return output;
 }
 
 /********** Public APIs **********/
 
 char *teapot_http_process(size_t *size, const char *input)
 {
-  // unimplemented
+    // test case
+    struct HttpResponse response =
+    {
+        HTTP_VERSION,
+        HTTP_STATUS_OK,
+        
+        "text/plain",
+        12,
+        "close",
+        NULL,
+        NULL,
+        
+        "Hello world!",
+    };
+  
+    char *response_str = teapot_http_response_construct(response);
+    // char *response_str = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+    *size = strlen(response_str);// < Record the length of the response string
+    return response_str;
 }
