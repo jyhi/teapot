dd87eb4639dd97cff023e0ee59ca2e1b8a09bc43
Junde Yhi <lmy441900@aosc.xyz>
2019-04-03 23:34:53 +0800

    http.c: fix redirection fall through

--- src/http.c
+++ src/http.c
@@ -489,10 +489,12 @@ char *teapot_http_process(size_t *size, const char *input)
           // If the new location is not temporart ->> HTTP 301
           response.status_code = HTTP_STATUS_MOVED_PERMANENTLY;
           response.location = teapot_redir_301_query(request.path);
+          break;
         } else if (teapot_redir_302_query(request.path) != NULL) {
           // If the new location is temporart ->> HTTP 302
           response.status_code = HTTP_STATUS_FOUND;
           response.location = teapot_redir_302_query(request.path);
+          break;
         }
 
         if (file == NULL) { // If the file does not exist.
